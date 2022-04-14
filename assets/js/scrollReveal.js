630327cdf89ef4a4ee19b9a7db57805d6e95c169
Gaitan Javier Emanuel <javii.gaitan@gmail.com>
2022-04-14 09:32:22 -0300

    Se agrega scrollReveal en index

---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+  sr.reveal('.alt', {
+      duration: 3000,
+      origin: 'bottom',
+      distance: '-100px'
+
+  });
+
+  sr.reveal('.logoIndexImg', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.features-row', {
+    duration: 3000,
+    origin: 'end',
+    distance: '-100px'
+  });
