278b8b560a3309a45e7b57969874c326e6791865
Gaitan Javier Emanuel <javii.gaitan@gmail.com>
2022-07-12 00:57:37 -0300

    Optimización de img, SEO

---
+++ assets/js/alert.js
@@ -0,0 +1,20 @@
+Swal.fire({
+    
+    icon: 'info',
+    html: 'Estas a un click de tener una Beca con nosotros <a href="https://wa.me/+5493541333382?text=Me%20gustaria%20tener%20mas%20informacion%20de%20sus%20cursos"></a>',
+    confirmButtonText:'Ver más',
+	padding: '1rem',
+	grow: 'row',
+	toast: true,
+	position: 'bottom',
+    timer: 10000,
+	timerProgressBar: true,
+    allowEscapeKey: false,
+	stopKeydownPropagation: false,
+
+
+	showConfirmButton: true,
+	showCancelButton: false,
+	showCloseButton: false,
+	closeButtonAriaLabel: 'Cerrar Promocion',
+  })
